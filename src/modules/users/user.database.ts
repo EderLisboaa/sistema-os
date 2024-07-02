a6e40fe749e0a951b80320077da8cd46f3e3425f
Eder Lisboa <ederrdls2@gmail.com>
2024-07-01 23:41:17 -0300

    use new sql connection to find user

--- src/modules/users/user.database.ts
+++ src/modules/users/user.database.ts
@@ -1,16 +1,14 @@
-import { connection as mysql } from "../../database/mysql";
+import { db } from "../../database/mysql";
 
 export class UserDatabase {
     constructor(){
 
     }
     
-    public async login (email: string, password: string): Promise<boolean> {
-        let query = `SELECT * FROM users WHERE email = ${email} AND password = ${password}`;
-        let user = await mysql.query(query);
+    public async login (email: string, password: string) {
+        const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+        const [queryResult, fieldPacket] = await db.query(query);
 
-        // console.log(user);
-
-        return true;
+        return queryResult;
     }
 }
